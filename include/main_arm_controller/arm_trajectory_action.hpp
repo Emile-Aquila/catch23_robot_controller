2357598a00af75b27798109887219da64aa107c7
Emile-Aquila <tmk8oakan6130@gmail.com>
2023-09-27 16:36:35 +0900

    impl arm trajectory action

--- include/main_arm_controller/arm_trajectory_action.hpp
+++ include/main_arm_controller/arm_trajectory_action.hpp
@@ -10,11 +10,16 @@
 #include <chrono>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
 #include <main_arm_controller/visibility.hpp>
-#include <catch23_robot_controller/srv/arm_trajectory_srv.hpp>
+#include <kondo_drivers/srv/kondo_b3m_srv.hpp>
+#include "std_msgs/msg/float32.hpp"
+
+#include <catch23_robot_controller/action/arm_trajectory_action.hpp>
 #include <catch23_robot_controller/msg/tip_state.hpp>
 #include <main_arm_controller/utils/robot_state.hpp>
 #include <main_arm_controller/trajectory/r_theta_optimal_planning.hpp>
+#include <main_arm_controller/utils/system_classes.hpp>
 
 
 namespace arm_trajectory_action{
@@ -24,9 +29,22 @@ namespace arm_trajectory_action{
         explicit ArmTrajectoryAction(const rclcpp::NodeOptions & options);
         ~ArmTrajectoryAction();
     private:
-        using arm_traj_srv = catch23_robot_controller::srv::ArmTrajectorySrv;
-        rclcpp::Service<arm_traj_srv>::SharedPtr arm_trajectory_service;
-        void srv_callback(const std::shared_ptr<arm_traj_srv::Request> request, const std::shared_ptr<arm_traj_srv::Response> response);
+        using traj_action = catch23_robot_controller::action::ArmTrajectoryAction;
+        using goal_handle_traj = rclcpp_action::ServerGoalHandle<traj_action>;
+
+        using kondo_msg = kondo_drivers::msg::B3mServoMsg;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr _pub_micro_ros_r, _pub_micro_ros_theta;
+        rclcpp::Publisher<kondo_msg>::SharedPtr _pub_b3m;
+        TrajectoryData _trajectory_data;
+
+        rclcpp_action::Server<traj_action>::SharedPtr arm_traj_action_server;
+        rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const traj_action::Goal> goal);
+        rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<goal_handle_traj> goal_handle);
+        void handle_accepted(const std::shared_ptr<goal_handle_traj> goal_handle);
+        void execute(const std::shared_ptr<goal_handle_traj> goal_handle);
+
+        void _convert_trajectory_data(const std::vector<catch23_robot_controller::msg::ArmState>& traj);
+        void _send_request_arm_state(const ArmState& req_arm_state);
 
         OMPL_PlannerClass planner;
     };
--- src/arm_trajectory_action.cpp
+++ src/arm_trajectory_action.cpp
@@ -9,57 +9,114 @@
 #include <rclcpp/rclcpp.hpp>
 #include <main_arm_controller/trajectory/r_theta_optimal_planning.hpp>
 #include <main_arm_controller/utils/robot_state.hpp>
+#include <main_arm_controller/utils/util_functions.hpp>
+
 
 
 namespace arm_trajectory_action{
+    const float min_r = 325.0f;
+    const float max_r = 975.0f;
+    const float min_theta = -M_PI;
+    const float max_theta = M_PI;
 
-    ArmTrajectoryAction::ArmTrajectoryAction(const rclcpp::NodeOptions &options):Node("traj_service", options) {
+    ArmState clip_arm_state(const ArmState& arm_state) {
+        ArmState ans;
+        ans.r = clip_f(arm_state.r, min_r, max_r);
+        ans.theta = clip_f(arm_state.theta, min_theta, max_theta);
+        ans.z = clip_f(arm_state.z, 0.0f, 225.0f);
+        ans.phi = clip_f(arm_state.phi, deg_to_rad(-97.0f), deg_to_rad(110.0f));
+        return ans;
+    }
+
+
+
+    ArmTrajectoryAction::ArmTrajectoryAction(const rclcpp::NodeOptions &options):Node("traj_action", options) {
         using namespace std::placeholders;
-        auto bind_callback = std::bind(&ArmTrajectoryAction::srv_callback, this, _1, _2);
-        arm_trajectory_service = this->create_service<arm_traj_srv>("arm_trajectory_service", bind_callback);
+        this->arm_traj_action_server = rclcpp_action::create_server<traj_action>(
+                this, "trajectory_action",
+                std::bind(&ArmTrajectoryAction::handle_goal, this, _1, _2),
+                std::bind(&ArmTrajectoryAction::handle_cancel, this, _1),
+                std::bind(&ArmTrajectoryAction::handle_accepted, this, _1));
+
+        _pub_b3m = this->create_publisher<kondo_msg>("b3m_topic", 10);
+        _pub_micro_ros_r = this->create_publisher<std_msgs::msg::Float32>("mros_input_r", 5);
+        _pub_micro_ros_theta = this->create_publisher<std_msgs::msg::Float32>("mros_input_theta", 5);
     }
-
-    void ArmTrajectoryAction::srv_callback(const std::shared_ptr<arm_traj_srv::Request> request,
-                                            const std::shared_ptr<arm_traj_srv::Response> response) {
-        if(request->waypoints.size() < 2){
-            RCLCPP_WARN(this->get_logger(), "[TrajService] invalid points num (points.size() < 2)");
-            response->is_feasible = false;
-            response->trajectory.clear();
-            return;
-        }
-
-        std::vector<ArmState> ans_traj;
-        bool is_feasible = true;
-        for(size_t i=0; i<request->waypoints.size()-1; i++){
-            TipState start = convert_tip_state(request->waypoints[i]), goal = convert_tip_state(request->waypoints[i+1]);
-            auto [traj_tmp, is_feasible_tmp] = this->planner.plan(
-                    start, goal,request->step_min, request->step_max, request->d_step_max, request->is_common);
-            if(!is_feasible_tmp)is_feasible = false;
-            ans_traj.insert(ans_traj.end(), traj_tmp.begin(), traj_tmp.end());
-        }
-        if(!is_feasible){
-            response->is_feasible = is_feasible;
-            response->trajectory.clear();
-            return;
-        }
-
-        std::ofstream writing_file;
-        try{
-            writing_file.open("path.txt");
-        }catch(const std::exception& e){
-            std::cerr << "can't open path.txt" << std::endl;
-        }
-        for(auto& tmp: ans_traj){
-            writing_file << tmp.theta << " " << tmp.r << " " << tmp.phi << std::endl;
-        }
-        writing_file.close();
-
-        std::transform(ans_traj.begin(), ans_traj.end(), std::back_inserter(response->trajectory),
-                       [](const auto& tmp){ return convert_arm_state(tmp); });
-        response->is_feasible = true;
-    }
-
     ArmTrajectoryAction::~ArmTrajectoryAction(){}
+
+    rclcpp_action::GoalResponse ArmTrajectoryAction::handle_goal(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const traj_action::Goal> goal) {
+        RCLCPP_INFO(this->get_logger(), "[TrajAction] Receive Trajectory Following Request");
+        (void) uuid;
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+
+    rclcpp_action::CancelResponse ArmTrajectoryAction::handle_cancel(const std::shared_ptr<goal_handle_traj> goal_handle) {
+        // serverにcancel要求が送信された時に呼び出される
+        RCLCPP_INFO(this->get_logger(), "[TrajAction] Received Cancel Trajectory Following Request");
+        (void) goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
+
+    void ArmTrajectoryAction::handle_accepted(const std::shared_ptr<goal_handle_traj> goal_handle) {
+        // serverがゴールを受け入れて実行を開始したときに呼び出される
+        using namespace std::placeholders;
+        // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+        std::thread{std::bind(&ArmTrajectoryAction::execute, this, _1), goal_handle}.detach();
+    }
+
+    void ArmTrajectoryAction::execute(const std::shared_ptr<goal_handle_traj> goal_handle) {
+        RCLCPP_INFO(this->get_logger(), "[TrajAction] Start Path Following");
+        rclcpp::Rate loop_rate(10);
+        const auto goal = goal_handle->get_goal();
+        auto feedback = std::make_shared<traj_action::Feedback>();
+        auto result = std::make_shared<traj_action::Result>();
+
+        this->_convert_trajectory_data(goal->trajectory);  // set trajectory
+        while(true) {
+            if ((!this->_trajectory_data.complete()) && rclcpp::ok()) {  // 経路追従中
+                if (goal_handle->is_canceling()) {  // cancel requestが来ているか確認
+                    goal_handle->canceled(result);
+                    RCLCPP_INFO(this->get_logger(), "[TrajAction] Path Following canceled");
+                    return;
+                }
+                this->_send_request_arm_state(this->_trajectory_data.get_front());
+                feedback->dummy = true;
+                goal_handle->publish_feedback(feedback);  // feedbackの送信
+                loop_rate.sleep();
+            }else{  // 経路追従完了
+                break;
+            }
+        }
+
+        if (rclcpp::ok()) {  // resultの処理
+            result->is_success = true;
+            goal_handle->succeed(result);
+            RCLCPP_INFO(this->get_logger(), "[TrajAction] Path Following succeeded");
+        }
+    }
+
+    void ArmTrajectoryAction::_convert_trajectory_data(const std::vector<catch23_robot_controller::msg::ArmState>& traj){
+        _trajectory_data.clear();
+        std::vector<ArmState> tmp_traj;
+        std::transform(traj.begin(), traj.end(), std::back_inserter(tmp_traj), [this](const auto& tmp){
+            ArmState arm_state = convert_arm_state(tmp);
+            if(clip_arm_state(arm_state) != arm_state){
+                RCLCPP_WARN(this->get_logger(), "****[WARN]**** [TrajAction] trajectory clipped! : (%lf, %lf, %lf)", arm_state.r, arm_state.theta, arm_state.phi);
+            }
+            return clip_arm_state(arm_state);
+        });
+        _trajectory_data.set(tmp_traj);
+    }
+
+    void ArmTrajectoryAction::_send_request_arm_state(const ArmState& req_arm_state){
+        uint8_t wrist_servo_id = 1;  // TODO: rosparam化
+        std_msgs::msg::Float32 tgt_data;
+        tgt_data.data = req_arm_state.r;
+        _pub_micro_ros_r->publish(tgt_data);
+        tgt_data.data = req_arm_state.theta;
+        _pub_micro_ros_theta->publish(tgt_data);
+        _pub_b3m->publish(gen_b3m_set_pos_msg(wrist_servo_id, -rad_to_deg(req_arm_state.phi), 0));
+    }
 };
 
 
